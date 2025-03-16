f2a72607317b86e54299076fb274120a683f27bd
kangeunchan <kangeunchan080310@gmail.com>
2025-03-17 08:18:54 +0900

    feat ( #6 ) : Add Operation Type ( enum class )

---
+++ src/main/kotlin/hs/kr/casper/configserver/domain/env/model/enum/EnvironmentOperationType.kt
@@ -0,0 +1,11 @@
+package hs.kr.casper.configserver.domain.env.model.enum
+
+enum class EnvironmentOperationType {
+    CREATE,
+    UPDATE,
+    DELETE,
+    BATCH_CREATE,
+    BATCH_UPDATE,
+    BATCH_DELETE,
+    PROFILE_DELETE
+}
