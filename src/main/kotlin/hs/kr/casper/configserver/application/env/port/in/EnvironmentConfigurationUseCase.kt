a17034edb2bb807e552916f0b937859a48539f9f
kangeunchan <kangeunchan080310@gmail.com>
2025-03-17 20:26:04 +0900

    feat ( #6 ) : Add application UseCase for single environment variable, multiple environment variables

---
+++ src/main/kotlin/hs/kr/casper/configserver/application/env/port/in/EnvironmentConfigurationUseCase.kt
@@ -0,0 +1,10 @@
+package hs.kr.casper.configserver.application.env.port.`in`
+
+import hs.kr.casper.configserver.adapter.`in`.env.dto.response.EnvironmentConfigurationResponse
+import hs.kr.casper.configserver.adapter.`in`.env.dto.response.EnvironmentOperationResponse
+
+interface EnvironmentConfigurationUseCase {
+    fun retrieveConfiguration(application: String, profile: String, label: String): EnvironmentConfigurationResponse
+    fun storeConfiguration(application: String, profile: String, label: String): EnvironmentOperationResponse
+    fun removeConfiguration(application: String, profile: String, label: String): EnvironmentOperationResponse
+}
---
+++ src/main/kotlin/hs/kr/casper/configserver/application/env/port/in/SingleEnvironmentValueUseCase.kt
@@ -0,0 +1,10 @@
+package hs.kr.casper.configserver.application.env.port.`in`
+
+import hs.kr.casper.configserver.adapter.`in`.env.dto.response.EnvironmentOperationResponse
+import hs.kr.casper.configserver.adapter.`in`.env.dto.response.EnvironmentValueResponse
+
+interface SingleEnvironmentValueUseCase {
+    fun retrieveValue(application: String, profile: String, label: String, key: String): EnvironmentValueResponse
+    fun storeValue(application: String, profile: String, label: String, key: String, value: String): EnvironmentOperationResponse
+    fun removeValue(application: String, profile: String, label: String, key: String): EnvironmentOperationResponse
+}
