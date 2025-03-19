b46be574bb6502f18dcdd6bb0a65a41fa56f123d
kangeunchan <kangeunchan080310@gmail.com>
2025-03-19 15:44:08 +0900

    feat ( #6 ) : Add EnvironmentConfiguration domain

---
+++ src/main/kotlin/hs/kr/casper/configserver/domain/env/model/EnvironmentConfiguration.kt
@@ -0,0 +1,14 @@
+package hs.kr.casper.configserver.domain.env.model
+
+import java.util.*
+
+data class EnvironmentConfiguration(
+    val id: UUID?,
+    val application: String,
+    val profile: String,
+    val label: String,
+    val key: String,
+    val value: String
+) {
+
+}
