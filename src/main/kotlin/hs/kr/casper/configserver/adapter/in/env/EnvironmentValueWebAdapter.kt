9ec24378ea5f77da5a565fc716512cef2c5345d0
kangeunchan <kangeunchan080310@gmail.com>
2025-03-23 22:21:20 +0900

    feat ( #6 ) : Implement environment configuration management with CRUD operations

--- src/main/kotlin/hs/kr/casper/configserver/adapter/in/env/EnvironmentValueWebAdapter.kt
+++ src/main/kotlin/hs/kr/casper/configserver/adapter/in/env/EnvironmentValueWebAdapter.kt
@@ -1,6 +1,14 @@
 package hs.kr.casper.configserver.adapter.`in`.env
 
-import hs.kr.casper.configserver.adapter.`in`.env.dto.request.EnvironmentValuesRequest
+import hs.kr.casper.configserver.adapter.`in`.env.dto.request.EnvironmentConfigurationRequest
+import hs.kr.casper.configserver.adapter.`in`.env.dto.response.EnvironmentConfigurationResponse
+import hs.kr.casper.configserver.adapter.`in`.env.dto.response.EnvironmentOperationResponse
+import hs.kr.casper.configserver.application.env.port.`in`.EnvironmentConfigurationUseCase
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,13 +18,94 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/env")
 class EnvironmentValueWebAdapter(
-
+    private val environmentConfigurationUseCase: EnvironmentConfigurationUseCase
 ) {
     @PostMapping
-    fun createEnvironmentValues(
+    fun storeEnvironmentConfiguration(
         @RequestBody
-        environmentValuesRequest: EnvironmentValuesRequest
-    ) : EnvironmentValuesRequest{
-        return environmentValuesRequest
+        environmentConfigurationRequest: EnvironmentConfigurationRequest
+    ): ResponseEntity<EnvironmentOperationResponse> {
+        val response = environmentConfigurationUseCase.storeConfiguration(
+            application = environmentConfigurationRequest.application,
+            profile = environmentConfigurationRequest.profile,
+            label = environmentConfigurationRequest.label,
+            properties = environmentConfigurationRequest.properties
+        )
+
+        return if (response.isSuccess) {
+            ResponseEntity.status(HttpStatus.CREATED).body(response)
+        } else {
+            ResponseEntity.status(HttpStatus.CONFLICT).body(response)
+        }
+    }
+
+    @GetMapping("/{application}/{profile}/{label}")
+    fun retrieveEnvironmentConfigurations(
+        @PathVariable
+        application: String,
+        @PathVariable
+        profile: String,
+        @PathVariable
+        label: String
+    ): EnvironmentConfigurationResponse {
+        return environmentConfigurationUseCase.retrieveEnvironmentConfigurations(
+            application = application,
+            profile = profile,
+            label = label
+        )
+    }
+
+    @GetMapping("/{application}/{profile}/{label}/{key}")
+    fun retrieveEnvironmentConfiguration(
+        @PathVariable
+        application: String,
+        @PathVariable
+        profile: String,
+        @PathVariable
+        label: String,
+        @PathVariable
+        key: String
+    ): EnvironmentConfigurationResponse {
+        return environmentConfigurationUseCase.retrieveEnvironmentConfiguration(
+            application = application,
+            profile = profile,
+            label = label,
+            key = key
+        )
+    }
+
+    @DeleteMapping("/{application}/{profile}/{label}")
+    fun removeEnvironmentConfigurations(
+        @PathVariable
+        application: String,
+        @PathVariable
+        profile: String,
+        @PathVariable
+        label: String
+    ) : EnvironmentOperationResponse{
+        return environmentConfigurationUseCase.removeConfigurations(
+            application,
+            profile,
+            label
+        )
+    }
+
+    @DeleteMapping("/{application}/{profile}/{label}/{key}")
+    fun removeEnvironmentConfiguration(
+        @PathVariable
+        application: String,
+        @PathVariable
+        profile: String,
+        @PathVariable
+        label: String,
+        @PathVariable
+        key: String
+    ) : EnvironmentOperationResponse{
+        return environmentConfigurationUseCase.removeConfiguration(
+            application,
+            profile,
+            label,
+            key
+        )
     }
 }
--- src/main/kotlin/hs/kr/casper/configserver/application/env/service/EnvironmentConfigurationService.kt
+++ src/main/kotlin/hs/kr/casper/configserver/application/env/service/EnvironmentConfigurationService.kt
@@ -1,28 +1,44 @@
 package hs.kr.casper.configserver.application.env.service
 
-import hs.kr.casper.configserver.adapter.`in`.env.dto.response.EnvironmentConfigurationsResponse
+import hs.kr.casper.configserver.adapter.`in`.env.dto.response.EnvironmentConfigurationResponse
 import hs.kr.casper.configserver.adapter.`in`.env.dto.response.EnvironmentOperationResponse
 import hs.kr.casper.configserver.application.env.port.`in`.EnvironmentConfigurationUseCase
+import hs.kr.casper.configserver.application.env.port.out.ExistsConfigurationPort
+import hs.kr.casper.configserver.application.env.port.out.RemoveConfigurationPort
 import hs.kr.casper.configserver.application.env.port.out.RetrieveConfigurationPort
 import hs.kr.casper.configserver.application.env.port.out.StoreConfigurationPort
 import hs.kr.casper.configserver.domain.env.model.EnvironmentConfiguration
 import hs.kr.casper.configserver.domain.env.model.enum.EnvironmentOperationType
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class EnvironmentConfigurationService(
     private val retrieveConfigurationPort: RetrieveConfigurationPort,
-    private val storeConfigurationPort: StoreConfigurationPort
+    private val storeConfigurationPort: StoreConfigurationPort,
+    private val removeConfigurationPort: RemoveConfigurationPort,
+    private val existsConfigurationPort: ExistsConfigurationPort
 ): EnvironmentConfigurationUseCase {
 
+    @Transactional
     override fun storeConfiguration(
         application: String,
         profile: String,
         label: String,
         properties: Map<String, String>
     ): EnvironmentOperationResponse {
+        val duplicateKeys = properties.keys.filter { key ->
+            existsConfigurationPort.existsConfiguration(application, profile, label, key)
+        }
 
-        properties.forEach { (key, value) ->
+        if (duplicateKeys.isNotEmpty()) {
+            return EnvironmentOperationResponse(
+                isSuccess = false,
+                operation = EnvironmentOperationType.STORE
+            )
+        }
+
+        properties.entries.forEach { (key, value) ->
             storeConfigurationPort.storeConfiguration(
                 EnvironmentConfiguration(
                     application = application,
@@ -35,24 +51,110 @@ class EnvironmentConfigurationService(
         }
 
         return EnvironmentOperationResponse(
-            success = true,
+            isSuccess = true,
             operation = EnvironmentOperationType.STORE
         )
     }
 
-    override fun removeConfiguration(
+    @Transactional(readOnly = true)
+    override fun retrieveEnvironmentConfigurations(
+        application: String,
+        profile: String,
+        label: String
+    ): EnvironmentConfigurationResponse {
+        val configurations = retrieveConfigurationPort.retrieveConfigurations(
+            application = application,
+            profile = profile,
+            label = label,
+        )
+
+        return EnvironmentConfigurationResponse(
+            application = application,
+            profile = profile,
+            label = label,
+            properties = configurations
+        )
+    }
+
+    @Transactional(readOnly = true)
+    override fun retrieveEnvironmentConfiguration(
+        application: String,
+        profile: String,
+        label: String,
+        key: String
+    ): EnvironmentConfigurationResponse {
+        val configuration = retrieveConfigurationPort.retrieveConfiguration(
+            application = application,
+            profile = profile,
+            label = label,
+            key = key
+        )
+
+        return EnvironmentConfigurationResponse(
+            application = application,
+            profile = profile,
+            label = label,
+            properties = configuration
+        )
+    }
+
+    @Transactional
+    override fun removeConfigurations(
         application: String,
         profile: String,
         label: String
     ): EnvironmentOperationResponse {
-        TODO("Not yet implemented")
+        val configurations = retrieveConfigurationPort.retrieveConfigurations(
+            application = application,
+            profile = profile,
+            label = label
+        )
+
+        configurations.keys.forEach { key ->
+            removeConfigurationPort.removeConfiguration(
+                EnvironmentConfiguration(
+                    application = application,
+                    profile = profile,
+                    label = label,
+                    key = key,
+                    value = configurations.getValue(key)
+                )
+            )
+        }
+
+        return EnvironmentOperationResponse(
+            isSuccess = true,
+            operation = EnvironmentOperationType.REMOVE
+        )
     }
 
-    override fun retrieveConfiguration(
+    @Transactional
+    override fun removeConfiguration(
         application: String,
         profile: String,
-        label: String
-    ): EnvironmentConfigurationsResponse {
-        TODO("Not yet implemented")
+        label: String,
+        key: String
+    ): EnvironmentOperationResponse {
+        val configuration = retrieveConfigurationPort.retrieveConfiguration(
+            application = application,
+            profile = profile,
+            label = label,
+            key = key
+        )
+
+        removeConfigurationPort.removeConfiguration(
+            EnvironmentConfiguration(
+                application = application,
+                profile = profile,
+                label = label,
+                key = key,
+                value = configuration[key]!!
+            )
+        )
+
+        return EnvironmentOperationResponse(
+            isSuccess = true,
+            operation = EnvironmentOperationType.REMOVE
+        )
     }
 }
