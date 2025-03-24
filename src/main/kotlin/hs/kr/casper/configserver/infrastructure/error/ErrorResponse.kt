5cc3eade7320d10ac1323751e981ccfb62daeaf6
kangeunchan <kangeunchan080310@gmail.com>
2025-03-24 10:23:11 +0900

    feat ( #10 ) : Introduce EntryException class for standardized error handling and update ErrorResponse structure

--- src/main/kotlin/hs/kr/casper/configserver/infrastructure/error/ErrorResponse.kt
+++ src/main/kotlin/hs/kr/casper/configserver/infrastructure/error/ErrorResponse.kt
@@ -1,6 +1,11 @@
 package hs.kr.casper.configserver.infrastructure.error
 
+import org.springframework.http.HttpStatus
+import java.time.LocalDateTime
+
 data class ErrorResponse(
-    val status: Int,
-    val message: String
+    val timestamp: LocalDateTime = LocalDateTime.now(),
+    val status: HttpStatus,
+    val error: String,
+    val path: String
 )
