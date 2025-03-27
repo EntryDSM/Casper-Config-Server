2ee214131938bb9fe9a82430dbf8890fb963ffd7
kangeunchan <kangeunchan080310@gmail.com>
2025-03-27 23:11:06 +0900

    feat : Update SQL query in application.yaml and enable config server in CasperConfigServerApplication

--- src/main/kotlin/hs/kr/casper/configserver/CasperConfigServerApplication.kt
+++ src/main/kotlin/hs/kr/casper/configserver/CasperConfigServerApplication.kt
@@ -2,8 +2,10 @@ package hs.kr.casper.configserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class CasperConfigServerApplication
 
 fun main(args: Array<String>) {
