85e74d9f63d90ba5db2799de6d208326ebeb7e9c
kangeunchan <kangeunchan080310@gmail.com>
2025-04-06 23:11:44 +0900

    feat ( #12 ) : Add EncryptionConfig with beans for encryption support

---
+++ src/main/kotlin/hs/kr/casper/configserver/infrastructure/encryption/EncryptionConfig.kt
@@ -0,0 +1,20 @@
+package hs.kr.casper.configserver.infrastructure.encryption
+
+import org.springframework.cloud.config.server.encryption.EncryptionController
+import org.springframework.cloud.config.server.encryption.TextEncryptorLocator
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.encrypt.TextEncryptor
+
+@Configuration
+class EncryptionConfig {
+    @Bean
+    fun textEncryptorLocator(encryptor: TextEncryptor): TextEncryptorLocator {
+        return TextEncryptorLocator { _: Map<String, String> -> encryptor }
+    }
+
+    @Bean
+    fun encryptionController(textEncryptorLocator: TextEncryptorLocator): EncryptionController {
+        return EncryptionController(textEncryptorLocator)
+    }
+}
