2f1af4dd6107f227563f0d7773feaf774c05a4e1
kangeunchan <kangeunchan080310@gmail.com>
2025-08-13 16:36:55 +0900

    chore: Enable bean definition overriding in application configuration

--- src/test/kotlin/hs/kr/casper/configserver/CasperConfigServerApplicationTests.kt
+++ src/test/kotlin/hs/kr/casper/configserver/CasperConfigServerApplicationTests.kt
@@ -2,8 +2,10 @@ package hs.kr.casper.configserver
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CasperConfigServerApplicationTests {
 
     @Test
